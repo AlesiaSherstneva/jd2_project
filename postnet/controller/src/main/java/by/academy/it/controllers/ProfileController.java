4933bafdb050978cdc54a703a6e9494fff41a75b
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-08 20:34:16 +0300

    Edit class

--- postnet/controller/src/main/java/by/academy/it/controllers/ProfileController.java
+++ postnet/controller/src/main/java/by/academy/it/controllers/ProfileController.java
@@ -11,7 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 import java.security.Principal;
