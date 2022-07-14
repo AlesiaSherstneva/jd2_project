4207d5ed71a8c558daa7424bf581f60cb5a582f1
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-14 15:27:38 +0300

    Edit initializer

--- postnet/view/src/main/java/by/academy/it/security/SecurityWebApplicationInitializer.java
+++ postnet/view/src/main/java/by/academy/it/security/SecurityWebApplicationInitializer.java
@@ -3,9 +3,5 @@ package by.academy.it.security;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-
-
-}
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {}
 
