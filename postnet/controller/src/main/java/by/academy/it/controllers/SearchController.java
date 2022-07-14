d475e342e580e48badd2fe2364cc9c1b9287aa4c
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-14 15:18:59 +0300

    Edit controller

--- postnet/controller/src/main/java/by/academy/it/controllers/SearchController.java
+++ postnet/controller/src/main/java/by/academy/it/controllers/SearchController.java
@@ -5,7 +5,10 @@ import by.academy.it.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
@@ -17,18 +20,18 @@ public class SearchController {
     @Autowired
     private UserDao userDao;
 
-    @GetMapping(value="/{page}")
+    @GetMapping(value = "/{page}")
     public String showUsers(@PathVariable int page, @RequestParam String searchString, Model model) {
         List<User> users = userDao.searchUsers(searchString);
-        if(users.size() == 0) {
+        if (users.size() == 0) {
             users = userDao.getAllUsers();
         }
         model.addAttribute("startpage", 1);
-        model.addAttribute("endpage", Math.ceil((double) users.size()/3));
+        model.addAttribute("endpage", Math.ceil((double) users.size() / 3));
         model.addAttribute("search", searchString);
         model.addAttribute("currentpage", page);
 
-        users = userDao.pageUsers(searchString, page-1);
+        users = userDao.pageUsers(searchString, page - 1);
         model.addAttribute("users", users);
         return "search/search-all";
     }
@@ -36,11 +39,10 @@ public class SearchController {
     @GetMapping("/user/{id}")
     public String showOneUser(@PathVariable("id") int id, Principal principal, Model model) {
         model.addAttribute("user", userDao.getUser(id));
-        if(principal!=null && !principal.getName().equals("admin")
+        if (principal != null && !principal.getName().equals("admin")
                 && id == userDao.getUserByEmail(principal.getName()).getId()) {
             return "redirect:/";
         }
         return "/search/search-one";
     }
-
 }
