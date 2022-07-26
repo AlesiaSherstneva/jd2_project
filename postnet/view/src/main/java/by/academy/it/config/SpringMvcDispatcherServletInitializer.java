6a7ed7cd90b147f4a6ecbff1a7644243f012f817
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-07-26 15:58:51 +0300

    Add filter for methods

--- postnet/view/src/main/java/by/academy/it/config/SpringMvcDispatcherServletInitializer.java
+++ postnet/view/src/main/java/by/academy/it/config/SpringMvcDispatcherServletInitializer.java
@@ -1,9 +1,12 @@
 package by.academy.it.config;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -27,6 +30,17 @@ public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationCon
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
         characterEncodingFilter.setEncoding("UTF-8");
         return new Filter[] { characterEncodingFilter};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 
 }
