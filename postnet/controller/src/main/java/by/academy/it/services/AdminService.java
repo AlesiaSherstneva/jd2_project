2a047fb96b77c53d04a4ffa48556678241112973
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-02 21:45:03 +0300

    Add some admin methods

--- postnet/controller/src/main/java/by/academy/it/services/AdminService.java
+++ postnet/controller/src/main/java/by/academy/it/services/AdminService.java
@@ -2,6 +2,7 @@ package by.academy.it.services;
 
 import by.academy.it.dao.UserDao;
 import by.academy.it.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,11 +11,24 @@ import java.util.List;
 public class AdminService {
     private final UserDao userDao;
 
+    @Autowired
     public AdminService(UserDao userDao) {
         this.userDao = userDao;
     }
 
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
+    }
+
+    public void banUser(int id) {
+        userDao.updateUserStatus((byte) 0, id);
+    }
+
+    public void unbanUser(int id) {
+        userDao.updateUserStatus((byte) 1, id);
+    }
+
+    public void deleteUser(int id) {
+        userDao.deleteUser(id);
     }
 }
