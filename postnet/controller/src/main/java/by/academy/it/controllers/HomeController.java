f5869d9e4890608267022e4bcacea7c54b3c8f03
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-02 21:05:26 +0300

    Add admin's and user's services to controller

--- postnet/controller/src/main/java/by/academy/it/controllers/HomeController.java
+++ postnet/controller/src/main/java/by/academy/it/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package by.academy.it.controllers;
 
-import by.academy.it.dao.UserDao;
+import by.academy.it.services.AdminService;
+import by.academy.it.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,12 +11,13 @@ import java.security.Principal;
 
 @Controller
 public class HomeController {
-
-    private final UserDao userDao;
+    private final UserService userService;
+    private final AdminService adminService;
 
     @Autowired
-    public HomeController(UserDao userDao) {
-        this.userDao = userDao;
+    public HomeController(UserService userService, AdminService adminService) {
+        this.userService = userService;
+        this.adminService = adminService;
     }
 
     @GetMapping("/login")
@@ -26,10 +28,10 @@ public class HomeController {
     @GetMapping("/")
     public String profile(Principal principal, Model model) {
         if (principal.getName().equals("admin")) {
-            model.addAttribute("users", userDao.getAllUsers());
+            model.addAttribute("users", adminService.getAllUsers());
             return "/administrate/admin-page";
         }
-        model.addAttribute("user", userDao.getUserByEmail(principal.getName()));
+        model.addAttribute("user", userService.getUserByEmail(principal.getName()));
         return "/registered/user-profile";
     }
 }
