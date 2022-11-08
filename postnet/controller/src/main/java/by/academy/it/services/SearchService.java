75a71b40675b59e4e7f43cb179eb09f2bb45cdea
Alesia Sherstneva <alesiasherstneva@gmail.com>
2022-11-08 14:32:23 +0300

    Create searching service

---
+++ postnet/controller/src/main/java/by/academy/it/services/SearchService.java
@@ -0,0 +1,30 @@
+package by.academy.it.services;
+
+import by.academy.it.dao.UserDao;
+import by.academy.it.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchService {
+    private final UserDao userDao;
+
+    @Autowired
+    public SearchService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public int pagesCount (String searchString) {
+        List<User> users = userDao.searchUsers(searchString);
+        if (users.isEmpty()) {
+            users = userDao.getAllUsers();
+        }
+        return users.size();
+    }
+
+    public List<User> pageUsers(String searchString, int page) {
+        return userDao.pageUsers(searchString, page);
+    }
+}
