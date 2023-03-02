278a4e0144c5a0a97e14fb303ca0849fc95e2c52
Alesia Sherstneva <alesiasherstneva@gmail.com>
2023-03-02 10:07:55 +0300

    Update custom validator (by senior's advice)

--- postnet/data/src/main/java/by/academy/it/validators/PasswordConstraintValidator.java
+++ postnet/data/src/main/java/by/academy/it/validators/PasswordConstraintValidator.java
@@ -4,24 +4,18 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class PasswordConstraintValidator implements ConstraintValidator<Password, String> {
-
-    String upperPattern = ".*[A-ZА-ЯЁ].*";
-    String lowerPattern = ".*[a-zа-яё].*";
-    String lengthPattern = ".{8}.*";
-    String digitPattern = ".*[0-9].*";
+    private final static String UPPER_PATTERN = ".*[A-ZА-ЯЁ].*";
+    private final static String LOWER_PATTERN = ".*[a-zа-яё].*";
+    private final static String DIGIT_PATTERN = ".*[0-9].*";
+    private final static String LENGTH_PATTERN = ".{8}.*";
 
     @Override
-    public void initialize(Password passwordAnnotation) {
-
-    }
+    public void initialize(Password passwordAnnotation) {}
 
     @Override
     public boolean isValid(String parameter, ConstraintValidatorContext context) {
-        try {
-            return parameter.matches(upperPattern) & parameter.matches(lowerPattern)
-                    & parameter.matches(lengthPattern) & parameter.matches(digitPattern);
-        } catch (NullPointerException exception) {
-            return true;
-        }
+        if (parameter == null) return true;
+        return parameter.matches(UPPER_PATTERN) & parameter.matches(LOWER_PATTERN)
+                    & parameter.matches(DIGIT_PATTERN) & parameter.matches(LENGTH_PATTERN);
     }
 }
